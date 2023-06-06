0bd108f7e2b8d2b81cf3587d6fc5ba40d27e6c47
psychowaspx <psychowaspx@gmail.com>
2023-06-06 10:30:48 +0200

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -86,8 +86,8 @@ class MyApp(App):
         return MainView()
 
     def trigger_full_ad(self):
-        #self.fullscreen_ad.show(self.fullscreen_delegate)
-        self.fullscreen_ad._show()
+        self.fullscreen_ad.show(self.fullscreen_delegate)
+    
 
     def toggle_banner(self, state):
         if state == "down":
